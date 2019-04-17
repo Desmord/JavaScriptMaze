cfb76768d521f8248db5b0813acede52228a0e34
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2019-04-17 17:52:22 +0200

    Create game generating method inside game object

--- dev/mainJs.js
+++ dev/mainJs.js
@@ -10,20 +10,45 @@ const game = {
     playerPosition: [1, 1],
     endPosition: [9, 9],
     numberOfPlayerMoves: 0,
-    generateGame(parentElement = `body`, width = `500px`) {
-        console.log(`Element rodzica: ${parentElement}`, `Szerokość: ${width}`);
+
+    generateGame(parentElement = `body`, width = `500`) {
+        // Setting game starting data
+        this.parentElement = parentElement;
+        this.width = width;
+        this.height = this.width * 1.1;
+
+        // Adding game to parent element
+        //tutaj pierw dodac poszczegule elementy do contenera a pozniej do rodzica
+        this.parentElement.appendChild(this.generatingMethods.createMainContainer(this.width, this.height));
+
     },
-    //
-    //
-    //
-    // generatingFunctions: {
-    //     wyswietl(hej = `gg`){
-    //         console.log(hej);
-    //     }
-    // }
-};
+
+    generatingMethods: {
+        /**
+         * Create and return game main container element
+         * @return {HTML element}
+         */
+        createMainContainer(width, height) {
+
+            let container = document.createElement(`div`);
+            container.style.width = `${width}px`;
+            container.style.height = `${height}px`;
+            container.style.backgroundColor = `rgba(52, 179, 168,0.9)`;
+
+            return container;
+
+        }
+
+        // create levevdificulty
+        // create start/ stop
+        // create gamePanel
+        // setGame - ustawia i wyswietla w elemencie partent
+
+    }
+
+}
 
 
 
-// game.generatingFunctions.wyswietl();
-// game.generateGame(`.content`,`700px`);
+
+game.generateGame(document.querySelector(`body`), `600`);
