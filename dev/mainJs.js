b7d541a53691af0fdea63a48644d3ca081354a22
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2019-04-17 15:54:31 +0200

    Create main game object with configuration data and game methods

--- dev/mainJs.js
+++ dev/mainJs.js
@@ -1,4 +1,29 @@
 import css from "./mainCss.css";
 
 
-console.log(`wita m`);
+const game = {
+    parentElement: null,
+    width: `500px`,
+    height: `600px`,
+    difficultyLevel: `easy`,
+    maze: [],
+    playerPosition: [1, 1],
+    endPosition: [9, 9],
+    numberOfPlayerMoves: 0,
+    generateGame(parentElement = `body`, width = `500px`) {
+        console.log(`Element rodzica: ${parentElement}`, `Szerokość: ${width}`);
+    },
+    //
+    //
+    //
+    // generatingFunctions: {
+    //     wyswietl(hej = `gg`){
+    //         console.log(hej);
+    //     }
+    // }
+};
+
+
+
+// game.generatingFunctions.wyswietl();
+// game.generateGame(`.content`,`700px`);
