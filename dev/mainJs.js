fbcb51f1d806bcf06e4bd969eaecd27ea7ff3445
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2019-04-18 17:09:51 +0200

    Initial display game html elements

--- dev/mainJs.js
+++ dev/mainJs.js
@@ -11,6 +11,11 @@ const game = {
     endPosition: [9, 9],
     numberOfPlayerMoves: 0,
 
+    /**
+     * Generate and add game element no site
+     * @param {HTML element} parentElement
+     * @param {number} width
+     */
     generateGame(parentElement = `body`, width = `500`) {
         // Setting game starting data
         this.parentElement = parentElement;
@@ -18,14 +23,26 @@ const game = {
         this.height = this.width * 1.1;
 
         // Adding game to parent element
-        //tutaj pierw dodac poszczegule elementy do contenera a pozniej do rodzica
-        this.parentElement.appendChild(this.generatingMethods.createMainContainer(this.width, this.height));
+        // Dodac metody sprawdzajace czy wartosc minimalna szewrokosci i wys jest spełniobna
+
+        let container = this.htmlElementsCreator.createMainContainer(this.width, this.height);
+        let div = document.createElement(`div`);
+
+        div.appendChild(this.htmlElementsCreator.createDifficultyLevelSelectElement(this.width, this.height));
+        div.appendChild(this.htmlElementsCreator.createStartGameElement(this.width, this.height));
+
+        container.appendChild(div);
+        container.appendChild(this.htmlElementsCreator.createGameBoardElement(this.width, this.height));
+
+        this.parentElement.appendChild(container);
 
     },
 
-    generatingMethods: {
+    htmlElementsCreator: {
         /**
          * Create and return game main container element
+         * @param {number} width
+         * @param {number} height
          * @return {HTML element}
          */
         createMainContainer(width, height) {
@@ -33,11 +50,69 @@ const game = {
             let container = document.createElement(`div`);
             container.style.width = `${width}px`;
             container.style.height = `${height}px`;
-            container.style.backgroundColor = `rgba(52, 179, 168,0.9)`;
+            container.style.backgroundColor = `rgba(52, 179, 168,0.3)`;
+            container.style.borderRadius = `20px`;
+            container.style.display = `flex`;
+            container.style.justifyContent = `center`;
+            container.style.alignItems = `center`;
+            container.style.flexDirection = `column`;
+
 
             return container;
 
+        },
+
+        /**
+         *  Create and return level difficulty select element
+         * @param {number} width
+         * @param {number} height
+         * @return {HTML element}
+         */
+        createDifficultyLevelSelectElement(width, height) {
+
+            let element = document.createElement(`select`);
+            element.style.width = `${width * 0.2}px`;
+            element.style.height = `${height * 0.05}px`;
+            element.style.backgroundColor = `rgba(255,255,255,1)`;
+            element.style.margin = `${width * 0.01}px ${width * 0.01}px `;
+
+            return element
+
+        },
+
+        /**
+         * Create and return start/stop button
+         * @param {number} width
+         * @param {number} height
+         * @return {HTML element}
+         */
+        createStartGameElement(width, height) {
+
+            let element = document.createElement(`div`);
+            element.style.width = `${width * 0.2}px`;
+            element.style.height = `${height * 0.05}px`;
+            element.style.backgroundColor = `rgba(255,255,255,1)`;
+            element.style.float = `right`;
+            element.style.margin = `${width * 0.01}px ${width * 0.01}px `;
+
+            return element
+
+
+        },
+
+        createGameBoardElement(width, height) {
+
+            let element = document.createElement(`div`);
+            element.style.width = `${width * 0.9}px`;
+            element.style.height = `${height * 0.9}px`;
+            element.style.backgroundColor = `rgba(255,255,255,1)`;
+            element.style.margin = `${width * 0.01}px ${width * 0.01}px `;
+
+            return element
+
+
         }
+
 
         // create levevdificulty
         // create start/ stop
